4395d03344dfa2f145f3d7863716d4379063fedc
CrispyChillies <pnhlong22@clc.fitus.edu.vn>
2026-01-29 16:59:59 +0700

    update statistic for mini datasets

--- settings.py
+++ settings.py
@@ -1,19 +1,19 @@
 base_architecture = 'vgg19'
 img_size = 224
-prototype_shape = (2000, 128, 1, 1)
-num_classes = 200
+prototype_shape = (200, 128, 1, 1)
+num_classes = 20
 prototype_activation_function = 'log'
 add_on_layers_type = 'regular'
 
 experiment_run = '003'
 
 data_path = '/kaggle/input/protopnet/datasets/cub200_cropped/'
-train_dir = data_path + 'train_cropped_augmented/'
-test_dir = data_path + 'test_cropped/'
-train_push_dir = data_path + 'train_cropped/'
-train_batch_size = 80
-test_batch_size = 100
-train_push_batch_size = 75
+train_dir = data_path + 'train_cropped_mini_augmented/'
+test_dir = data_path + 'test_cropped_mini/'
+train_push_dir = data_path + 'train_cropped_mini/'
+train_batch_size = 32
+test_batch_size = 32
+train_push_batch_size = 32
 
 joint_optimizer_lrs = {'features': 1e-4,
                        'add_on_layers': 3e-3,
@@ -28,11 +28,11 @@ last_layer_optimizer_lr = 1e-4
 coefs = {
     'crs_ent': 1,
     'clst': 0.8,
-    'sep': -0.08,
-    'l1': 1e-4,
+    'sep': -0.15,
+    'l1': 1e-5,
 }
 
-num_train_epochs = 1000
+num_train_epochs = 100
 num_warm_epochs = 5
 
 push_start = 10
